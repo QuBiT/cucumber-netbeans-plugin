8e7d424aef4fc4e4d923b122ffb1426240b07ecb
QuBiT <Roland@.(none)>
2009-06-05 16:50:31 +0200

    Version 1.1.8 (Adding Icons)

--- src/info/cukes/feature/Feature.java
+++ src/info/cukes/feature/Feature.java
@@ -1,50 +1,34 @@
 package info.cukes.feature;
 
-import org.netbeans.api.languages.ASTNode;
-import org.netbeans.api.languages.ASTPath;
 import org.netbeans.api.languages.SyntaxContext;
 
 public class Feature {
 
     public static String scenarioName(SyntaxContext context) {
-        ASTPath path = context.getASTPath();
-        ASTNode node = (ASTNode) path.getLeaf();
-        node = node.getNode("Scenario->ScenarioDefinition->ScenarioDescription->Text->Words");
-        if (node != null) {
-            return "Does not work.";
-        }
         return "Scenario";
     }
 
     public static String featureName(SyntaxContext context) {
-        ASTPath path = context.getASTPath();
-        ASTNode node = (ASTNode) path.getLeaf();
-        node = node.getNode("Feature->FeatureDefinition->FeatureDescription->Text->Words");
-        if (node != null) {
-            return "Does not work.";
-        }
         return "Feature";
+    }
+    
+    public static String backgroundName(SyntaxContext context) {
+        return "Background";
+    }
+
+    public static String backgroundIcon(SyntaxContext context) {
+        return "info/cukes/feature/resources/background.png";
     }
 
     public static String scenarioIcon(SyntaxContext context) {
-        ASTPath path = context.getASTPath();
-        ASTNode node = (ASTNode) path.getLeaf();
-        node = node.getNode("Scenario");
-        if (node != null) {
-            return "/org/netbeans/modules/languages/resources/variable.gif";
-        }
-
-        return "/org/netbeans/modules/languages/resources/method.gif";
+        return "info/cukes/feature/resources/scenario.png";
     }
 
     public static String featureIcon(SyntaxContext context) {
-        ASTPath path = context.getASTPath();
-        ASTNode node = (ASTNode) path.getLeaf();
-        node = node.getNode("Feature");
-        if (node != null) {
-            return "/org/netbeans/modules/languages/resources/variable.gif";
-        }
+        return "/info/cukes/feature/resources/cucumber.png";
+    }
 
-        return "/org/netbeans/modules/languages/resources/method.gif";
+    public static String examplesIcon(SyntaxContext context) {
+        return "/info/cukes/feature/resources/limwire.png";
     }
 }

6eea859cd53284493e5cf2fc2c852b6c057a7f87
QuBiT <Roland@.(none)>
2009-07-03 15:23:49 +0200

    improved navigator (keyword+text)

--- src/info/cukes/feature/Feature.java
+++ src/info/cukes/feature/Feature.java
@@ -1,18 +1,54 @@
 package info.cukes.feature;
 
+import org.netbeans.api.languages.ASTNode;
+import org.netbeans.api.languages.ASTPath;
 import org.netbeans.api.languages.SyntaxContext;
 
 public class Feature {
 
-    public static String scenarioName(SyntaxContext context) {
-        return "Scenario";
+    private static ASTNode getActualNode(SyntaxContext context) {
+        ASTPath path = context.getASTPath();
+        return (ASTNode) path.getLeaf();
     }
 
     public static String featureName(SyntaxContext context) {
+        ASTNode node = getActualNode(context);
+        node = node.getNode("FeatureDefinition");
+        if (node != null) {
+            node = node.getNode("FeatureDescription");
+            if (node != null) {
+                return "Feature: " + node.getAsText().trim();
+            }
+        }
         return "Feature";
     }
 
+    public static String scenarioName(SyntaxContext context) {
+        ASTNode node = getActualNode(context);
+        ASTNode snode = node.getNode("BasicScenario");
+        if (snode != null) {
+            node = snode.getNode("ScenarioDescription");
+            if (node != null) {
+                return "Scenario: " + node.getAsText().trim();
+            }
+        } else {
+            snode = node.getNode("TableScenario");
+            if (snode != null) {
+                node = snode.getNode("ScenarioDescription");
+                if (node != null) {
+                    return "Scenario Outline: " + node.getAsText().trim();
+                }
+            }
+        }
+        return "Scenario";
+    }
+
     public static String backgroundName(SyntaxContext context) {
+        ASTNode node = getActualNode(context);
+        node = node.getNode("Description");
+        if (node != null) {
+            return "Background: " + node.getAsText().trim();
+        }
         return "Background";
     }
 
